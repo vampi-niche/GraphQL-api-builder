9d2d118c721e7e5980bb67651d6e76521ac38c75
vampi-niche <vampi.niche@gmail.com>
2023-11-18 23:07:56 -0700

    setup

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const graphqlHttp = require('express-graphql').graphqlHTTP
 
 const app = express()
+
+app.use('/graphql', graphqlHttp({
+
+}))
 
 app.listen(4000,()=>{
     console.log('Listening on :4000')
