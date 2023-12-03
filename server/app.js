1d57b70080668c3a2abbfea39a1b70579a78d04f
vampi-niche <vampi.niche@gmail.com>
2023-12-03 16:44:34 -0700

    server

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const graphqlHttp = require('express-graphql').graphqlHTTP
+const schema = require('./schema/schema')
 
 const app = express()
 
 app.use('/graphql', graphqlHttp({
+    schema,
+    graphiql:true
 
 }))
 
-app.listen(4000,()=>{
-    console.log('Listening on :4000')
+app.listen(4001,()=>{
+    console.log('Listening on :4001')
 })
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,15 @@
 const graphql = require('graphql')
 
-const {GraphQLObjectType, GraphQLString} = graphql
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql
+
+//dummy data
+
+const book = [
+    {name: 'book_1', genre: 'genre_1', id: '1'},
+    {name: 'book_2', genre: 'genre_2', id: '2'},
+    {name: 'book_3', genre: 'genre_3', id: '3'},
+
+]
 
 const BookType = new GraphQLObjectType({
     name:'Book',
@@ -19,7 +28,10 @@ const RootQuery = new GraphQLObjectType({
             args:{id:{type:GraphQLString}},
             resolve(parent, args){
                 //getData
+                return book.find(e => e.id === args.id)
             }
         }
     }
 })
+
+module.exports = new GraphQLSchema({query: RootQuery})
