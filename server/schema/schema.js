2c6eb9c03e0078a1d589daabf7957f1a9a9bee45
vampi-niche <vampi.niche@gmail.com>
2023-12-03 16:52:29 -0700

    root_author_accessible

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,22 +1,37 @@
 const graphql = require('graphql')
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt} = graphql
 
 //dummy data
 
-const book = [
+const books = [
     {name: 'book_1', genre: 'genre_1', id: '1'},
     {name: 'book_2', genre: 'genre_2', id: '2'},
     {name: 'book_3', genre: 'genre_3', id: '3'},
 
 ]
 
+const authors = [
+    {name: 'author_1', age: 21, id: '1'},
+    {name: 'author_2', age: 22, id: '2'},
+    {name: 'author_3', age: 23, id: '3'},
+
+]
+
 const BookType = new GraphQLObjectType({
     name:'Book',
     fields:()=>({
-        id:{type:GraphQLString},
+        id:{type:GraphQLID},
         name:{type:GraphQLString},
         genre:{type:GraphQLString}
+    })
+})
+const AuthorType = new GraphQLObjectType({
+    name:'Author',
+    fields:()=>({
+        id:{type:GraphQLID},
+        name:{type:GraphQLString},
+        age:{type:GraphQLInt}
     })
 })
 
@@ -25,12 +40,21 @@ const RootQuery = new GraphQLObjectType({
     fields:{
         book:{
             type:BookType,
-            args:{id:{type:GraphQLString}},
+            args:{id:{type:GraphQLID}},
             resolve(parent, args){
                 //getData
-                return book.find(e => e.id === args.id)
+                return books.find(e => e.id === args.id)
+            }
+        },
+        author:{
+            type:AuthorType,
+            args:{id:{type:GraphQLID}},
+            resolve(parent, args){
+                //getData
+                return authors.find(e => e.id === args.id)
             }
         }
+
     }
 })
 
